26245f150f2fb4005a09073a8faeff5ef298e015
SagaLGIT <saga.lundborg@gmail.com>
2024-07-25 17:13:54 -0700

    server structure start

--- server.js
+++ server.js
@@ -12,8 +12,30 @@ app.use(
     )
 )
 
+const inputVoice = "echo"; // https://platform.openai.com/docs/guides/text-to-speech/voice-options
+const inputModel = "tts-1"; // https://platform.openai.com/docs/guides/text-to-speech/audio-quality
+
 app.post("/synthesize", async (req, res) => {
     const text = req.body.text
     const apiKey = "dummy"
-    const endpoint = ''
+    const endpoint = "https://api.openai.com/v1/audio/speech"
+    // Request body 
+    const payload = {
+        text: text,
+        model: inputModel,
+        accept: inputVoice,
+        response_format: "mp3",
+    }
+
+    const response = await axios.post(endpoint, payload, {
+        headers: headers,
+        responseType: "stream",
+      })
+    res.json(response.data)
 })
+
+const_posrt = 3001
+app.listen(const_posrt, () => {
+    console.log(`Server is running on port ${const_posrt}`)
+})
+
